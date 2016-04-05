b0dcbd180eb5e166290adbd369e4c57b1d7f6048
Ehsan Ziya <ehsan.ziya@gmail.com>
2016-04-05 22:03:58 +0100

    Refactor: Move voice to its own module and more clean up in the index

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ var pt = require('ptjs');
 var _ = require('lodash');
 var teoria = require('teoria');
 var randomInt = require('random-int');
+var uuid = require('node-uuid');
 
-var envelope = require('./lib/envelope');
+var Voice = require('./lib/voice');
 var context = require('./lib/context');
 var load = require('./lib/load');
+
 var limiter = context.createDynamicsCompressor();
 var convolver = context.createConvolver();
+var synthGain = context.createGain();
+
 limiter.ratio.value = 40;
 limiter.attack.value = 0.01;
 limiter.release.value = 0.01;
 limiter.threshold.value = -1;
+
 limiter.connect(context.destination);
 convolver.connect(limiter);
+synthGain.connect(convolver);
 
-load('./assets/ir.mp3', function(buffer) {
+load('./assets/ir.mp3', function (buffer) {
   convolver.buffer = buffer;
 });
 
@@ -25,10 +31,12 @@ var orange = '#FF9F1C';
 var lightBlue = '#2EC4B6';
 var red = '#E71D36';
 
-var space = new pt.CanvasSpace('canvas', darkNavyBlue).display('#pt', function() {}, true);
+var space = new pt.CanvasSpace('canvas', darkNavyBlue).display('#pt', function () {}, true);
 space.autoResize(true);
-
 var form = new pt.Form(space);
+
+var dot = new pt.Circle(250, 250).setRadius(space.size.x / 13);
+var smallDot = new pt.Circle().setRadius(3);
 
 var g3 = teoria.note('g3');
 var scale = g3.scale('minorpentatonic');
@@ -37,77 +45,62 @@ var notes = scale.notes();
 var currentlyPlaying = [];
 var currentPoints = [];
 
-function change(toAdd, toRemove) {
-  console.log(toAdd, toRemove);
+function createPoints(amount) {
+  var points = [];
+  for (var i = 0; i < amount; i++) {
+    var point = new pt.Vector(Math.random() * space.size.x, Math.random() * space.size.y);
+    var randomNote = notes[randomInt(0, notes.length - 1)];
+    var multipliers = [1, 0.5, 2];
 
-  toRemove.forEach(function(point) {
-    var now = context.currentTime;
-    point.gain.gain.cancelScheduledValues(now);
-    point.gain.gain.setValueAtTime(point.gain.gain.value, now);
-    point.gain.gain.exponentialRampToValueAtTime(0.000001, now + 5);
-    point.gain.gain.setValueAtTime(0, now + 5.1);
-    point.osc.stop(now + 6.4);
-    setTimeout(function() {
-      point.gain.disconnect();
-    }, 7000);
-    var indexToDelete = currentlyPlaying.indexOf(point);
+    point.fq = randomNote.fq() * multipliers[randomInt(0, 2)];
+    point.id = uuid.v1();
+    points.push(point);
+  }
+  return points;
+}
+
+var points = createPoints(80);
+
+
+function change(toAdd, toRemove) {
+  var intersected = _.intersectionBy(currentlyPlaying, toRemove, 'id');
+
+  intersected.forEach(function (voice) {
+    voice.stop({
+      now: context.currentTime,
+      release: 5
+    });
+    var indexToDelete = currentlyPlaying.indexOf(voice);
     currentlyPlaying.splice(indexToDelete, 1);
   });
 
-  toAdd.forEach(function(point) {
-    var osc = context.createOscillator();
-    var gain = context.createGain();
-
-    osc.connect(gain);
-    gain.connect(convolver);
-
-    osc.frequency.value = point.fq;
-    osc.start(0);
-    envelope(gain.gain, context.currentTime, {
-      start: 0,
+  toAdd.forEach(function (point) {
+    var voice = new Voice(point.id, point.fq, synthGain);
+    voice.start({
+      now: context.currentTime,
       peak: 0.03,
-      attack: 4,
-      type: 'exponential'
+      attack: 6
     });
-
-    point.osc = osc;
-    point.gain = gain;
-    currentlyPlaying.push(point);
+    currentlyPlaying.push(voice);
   });
-
-  console.log('current notes', currentlyPlaying.length);
 }
 
-var dot = new pt.Circle(250, 250).setRadius(space.size.x / 13);
-var smallDot = new pt.Circle().setRadius(3);
-var points = [];
-
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   dot.radius = space.size.x / 13;
-  points.forEach(function(point) {
-    point.set(Math.random() * space.size.x, Math.random() * space.size.y);
-  });
+  points = createPoints(80);
 });
 
-window.addEventListener('mousemove', function(evt) {
+window.addEventListener('mousemove', function (evt) {
   if (evt.target.id !== 'pt') {}
 });
 
-for (var i = 0; i < 70; i++) {
-  var point = new pt.Vector(Math.random() * space.size.x, Math.random() * space.size.y);
-  var randomNote = notes[randomInt(0, notes.length - 1)];
-  var multipliers = [1, 0.5, 2];
-
-  point.fq = randomNote.fq() * multipliers[randomInt(0, 2)];
-  points.push(point);
-}
 
 var bot = {
-  animate: function(time, fs, context) {
+  animate: function (time, fs, context) {
     form.fill(backgroundColor).stroke(false);
     form.circle(dot);
     var pointsInsideCircle = [];
-    points.forEach(function(pt) {
+    points.forEach(function (pt) {
       pt.set(pt.x + (Math.random() - 0.5), pt.y + (Math.random() - 0.5));
       var intersected = dot.intersectPoint(pt);
       if (intersected) {
@@ -126,8 +119,8 @@ var bot = {
       space.space.style.cursor = 'auto';
     }
 
-    pointsInsideCircle.forEach(function(point, index) {
-      pointsInsideCircle.forEach(function(point2, index2) {
+    pointsInsideCircle.forEach(function (point, index) {
+      pointsInsideCircle.forEach(function (point2, index2) {
         if (index === index2) return;
         form.stroke(orange);
         var line = new pt.Line(point).to(point2);
@@ -138,24 +131,23 @@ var bot = {
     if (!_.isEqual(currentPoints, pointsInsideCircle)) {
       var toRemove = _.difference(currentPoints, pointsInsideCircle);
       var toAdd = _.difference(pointsInsideCircle, currentPoints);
-      console.log(toAdd, toRemove);
       change(toAdd, toRemove);
     }
     currentPoints = pointsInsideCircle;
   },
-  onMouseAction: function(type, x, y) {
+  onMouseAction: function (type, x, y) {
     if (type === 'move') {
       dot.set(x, y);
     }
   },
-  onTouchAction: function(type, x, y, evt) {
+  onTouchAction: function (type, x, y, evt) {
     if (type === 'move') {
       var offsetX = (window.innerWidth - evt.target.offsetWidth) / 2;
       dot.set(x - offsetX, y);
     }
     evt.preventDefault();
   }
-}
+};
 
 space.add(bot);
 space.bindMouse();
